4bd3cd917c5e17d805aad04fa04c2b5c0e1dd114
ksx0330 <ksx0330@naver.com>
2017-08-01 00:25:44 +0900

    IP Check Complete

--- arp_spoofing.c
+++ arp_spoofing.c
@@ -6,8 +6,11 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <net/if.h>
+
 
 struct sniff_ethernet {
         u_char  ether_dhost[6];    /* destination host address */
@@ -35,13 +38,87 @@ struct sniff_ip {
 #define IP_V(ip)                (((ip)->ip_vhl) >> 4)
 
 struct sniff_arp {
-	u_short arp_htype; /*hardware type*/
-	u_short arp_p; /*protocol*/
-	u_char arp_hsize; /*hardware size*/
-	u_char arp_psize; /*protocol size*/
-	u_short arp_opcode; /*opcode*/
-	u_char arp_smhost[6]; /*sender mac address*/
-	struct in_addr arp_sip; /*sender ip address*/
-	u_char arp_dmhost[6]; /*target mac address*/
-	struct in_addr arp_dip; /*target ip address*/
+        u_short arp_htype; /*hardware type*/
+        u_short arp_p; /*protocol*/
+        u_char arp_hsize; /*hardware size*/
+        u_char arp_psize; /*protocol size*/
+        u_short arp_opcode; /*opcode*/
+        u_char arp_smhost[6]; /*sender mac address*/
+        struct in_addr arp_sip; /*sender ip address*/
+        u_char arp_dmhost[6]; /*target mac address*/
+        struct in_addr arp_dip; /*target ip address*/
 };
+
+int s_getIpAddress (const char * ifr, unsigned char * out) {  
+    int sockfd;  
+    struct ifreq ifrq;  
+    struct sockaddr_in * sin;  
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);  
+    strcpy(ifrq.ifr_name, ifr);  
+    if (ioctl(sockfd, SIOCGIFADDR, &ifrq) < 0) {  
+        perror( "ioctl() SIOCGIFADDR error");  
+        return -1;  
+    }  
+    sin = (struct sockaddr_in *)&ifrq.ifr_addr;  
+    memcpy (out, (void*)&sin->sin_addr, sizeof(sin->sin_addr));  
+  
+    close(sockfd);  
+  
+    return 4;  
+} 
+
+int main(int argc, char **argv) {
+        char arp_packet[42];
+        char *dev = NULL;
+        char errbuf[PCAP_ERRBUF_SIZE];
+        pcap_t *handle;
+
+        struct in_addr addr;
+        char *maskp;
+        char *netp;
+
+        bpf_u_int32 mask;
+        bpf_u_int32 net;
+
+        struct ifreq ifr;
+        char ipstr[40];
+        int s;
+
+        if (argc == 2) {
+                dev = argv[1];
+        } else if (argc > 2) {
+                fprintf(stderr, "error: unrecognized command-line options\n");
+        } else {
+                dev = pcap_lookupdev(errbuf);
+                if (dev == NULL) {
+                        fprintf(stderr, "Couldn't find default deviceL %s\n", errbuf);
+                        exit(EXIT_FAILURE);
+                }
+        }
+
+        if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+                fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+                net = 0;
+                mask = 0;
+        }
+
+        addr.s_addr = net;
+        netp = inet_ntoa(addr);
+
+        printf("IP : %s\n", netp);
+
+        addr.s_addr = mask;
+        maskp = inet_ntoa(addr);
+
+        printf("MASK : %s\n", maskp);
+
+        printf("Device: %s\n", dev);
+
+    unsigned char addrs[4] = {0,};  
+  
+    if (s_getIpAddress(dev, addrs) > 0) {  
+        printf("ip addr:=%d.%d.%d.%d", (int)addrs[0], (int)addrs[1], (int)addrs[2], (int)addrs[3]);  
+    }  
+
+
+}
